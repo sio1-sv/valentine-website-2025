44eb992c068c3e766539c5b1bd4931765cffc562
sio1-sv <sionajohnezekyle@gmail.com>
2026-02-12 01:34:01 +0800

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 const CONFIG = {
     // Your Valentine's name that will appear in the title
     // Example: "Jade", "Sarah", "Mike"
-    valentineName: "Angela my baby",
+    valentineName: "Angela",
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
