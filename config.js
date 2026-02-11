b37dbe687047723ae097041562874e18976bcd2b
sio1-sv <sionajohnezekyle@gmail.com>
2026-02-12 01:57:20 +0800

    Update config.js

--- config.js
+++ config.js
@@ -50,8 +50,8 @@ const CONFIG = {
     // Messages that appear after they say "Yes!"
     celebration: {
         title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "Now come get your gift, a big warm hug and a huge kiss!",
-        emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
+        message: "Now come here baby give me a kiss !",
+        emojis: "💖🤗💝💋❤️💕"  // These will bounce around
     },
 
     // Color scheme for the website
